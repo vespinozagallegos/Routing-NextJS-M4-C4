08a7ef11940ce1cd24c808616691b86713e5f5be
vespinozagallegos <espinozagallegosvictor@gmail.com>
2025-01-19 21:49:05 -0300

    Se crea page que es LA RUTA que renderiza el componente ESTATICA

---
+++ routing-next/src/app/estatica/Estatica.jsx
@@ -0,0 +1,11 @@
+
+// Componente que se verá en la RUTA
+export const Estatica = () => {
+    return (
+        <div>
+            Estamos en nuestra Ruta Estatica: https://localhost:3000/estatica
+        </div>
+    );
+}
+
+export default Estatica
