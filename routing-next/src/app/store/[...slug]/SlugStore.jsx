f225c2865db18d4ac87810cfdc93baad8e8fc7f1
vespinozagallegos <espinozagallegosvictor@gmail.com>
2025-01-21 17:22:34 -0300

    Se agrega el destructuring de params y captura los parametros que les agregemos como dice en los comentarios. Falta que veamos una diferencia entre agregar 1 params y mas de 1

--- routing-next/src/app/store/[...slug]/SlugStore.jsx
+++ routing-next/src/app/store/[...slug]/SlugStore.jsx
@@ -1,7 +1,15 @@
-export const SlugStore = () => {
+// Aceeder a los params desestructurando params
+// params es un objeto PERO esta vez necesitamos MAS de un PARAMS
+// si dejamos { params } podemos agregar mil slashes diferentes
+// store/products//shirts o store/category/subcategory...
+// Y renderiza el MISMO retorno <h1>Este es SlugStore</h1>
+
+// Para hacer la diferencia debemos
+// agregar como array de strings { slug: ["category", "subcategory"] }
+export const SlugStore = ({ params }) => {
     return(
         <div>
-            <h1>SlugStore</h1>
+            <h1>Este es SlugStore</h1>
         </div>
     );
 }
